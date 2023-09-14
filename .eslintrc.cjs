76dc92b8627b1c62adc17b42f43cead82292c07d
lmh <1121376989@qq.com>
2023-09-14 14:35:08 +0800

    feat: prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,52 +1,51 @@
 module.exports = {
-	'env': {
-		'browser': true,
-		'es2021': true,
-		'node': true
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
 	},
-	'extends': [
+	extends: [
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended',
-		'plugin:vue/vue3-essential'
+		'plugin:vue/vue3-essential',
+		'plugin:prettier/recommended',
 	],
-	'overrides': [
+	overrides: [
 		{
-			'env': {
-				'node': true
+			env: {
+				node: true,
 			},
-			'files': [
-				'.eslintrc.{js,cjs}'
-			],
-			'parserOptions': {
-				'sourceType': 'script'
-			}
-		}
+			files: ['.eslintrc.{js,cjs}'],
+			parserOptions: {
+				sourceType: 'script',
+			},
+		},
 	],
-	'parserOptions': {
-		'ecmaVersion': 'latest',
-		'parser': '@typescript-eslint/parser',
-		'sourceType': 'module'
+	parserOptions: {
+		ecmaVersion: 'latest',
+		parser: '@typescript-eslint/parser',
+		sourceType: 'module',
 	},
-	'plugins': [
-		'@typescript-eslint',
-		'vue'
-	],
-	'rules': {
-		'indent': [
+	plugins: ['@typescript-eslint', 'vue'],
+	rules: {
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'windows'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always'],
+		'prettier/prettier': [
 			'error',
-			'tab'
+			{
+				printWidth: 80,
+				tabWidth: 2,
+				useTabs: true,
+				singleQuote: true,
+				semi: true,
+				trailingComma: 'es5',
+				bracketSpacing: true,
+				jsxBracketSameLine: false,
+				arrowParens: 'avoid',
+				endOfLine: 'crlf',
+			},
 		],
-		'linebreak-style': [
-			'error',
-			'windows'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'always'
-		]
-	}
+	},
 };
